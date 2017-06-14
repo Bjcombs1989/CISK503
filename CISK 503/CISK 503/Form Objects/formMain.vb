188a419b51e88846f101a087b0c4371ceffd1e8b
Bjcombs1989 <bjcombs@gmail.com>
2017-06-13 20:29:54 -0500

    Added main form tab control and menu strip, tested MySQL connection from my home IP address.

--- CISK 503/CISK 503/Form Objects/formMain.vb
+++ CISK 503/CISK 503/Form Objects/formMain.vb
@@ -1,3 +1,20 @@
 ﻿Public Class formMain
+    Dim mysql As MySQLDatabaseConnector
+
+    Private Sub formMain_Load() Handles Me.Load
+        ' This code hides the tabs at runtime
+        TabControl1.Appearance = TabAppearance.FlatButtons
+        TabControl1.ItemSize = New Size(0, 1)
+        TabControl1.SizeMode = TabSizeMode.Fixed
+
+        ' Load the Database Connection
+        Try
+            mysql = New MySQLDatabaseConnector()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
 
 End Class
