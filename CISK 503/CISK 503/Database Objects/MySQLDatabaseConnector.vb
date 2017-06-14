117cf74d03f81482104918fd2caaf0a625311980
Bjcombs1989 <bjcombs@gmail.com>
2017-06-13 20:21:03 -0500

    Replaced MDF with MySQL Database Connector

---
+++ CISK 503/CISK 503/Database Objects/MySQLDatabaseConnector.vb
@@ -0,0 +1,52 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class MySQLDatabaseConnector
+    Dim conn As New MySqlConnection
+    Dim DatabaseName As String = "combsan1_tamuct"
+    Dim server As String = "www.combsandcombs.com"
+    Dim userName As String = "combsan1_tamuct"
+    Dim password As String = "gii{M[Ag$=gb"
+
+    ' Constructor
+    Public Sub New()
+        ' Start by making sure the conn is closed
+        If Not conn Is Nothing Then conn.Close()
+
+        ' Create the connection string
+        conn.ConnectionString = String.Format("server={0}; user id={1}; password={2}; database={3}; pooling=false", server, userName, password, DatabaseName)
+
+        ' Try to open the connection
+        Try
+            conn.Open()
+        Catch ex As Exception
+            Throw ex
+        End Try
+
+
+
+    End Sub
+
+    ' Methods
+    Sub ExecuteNonQuery(mySQL As String)
+        Dim cmd As New MySqlCommand(mySQL, conn)
+        cmd.ExecuteNonQuery()
+    End Sub
+
+    Sub AddNewUser(username As String, password As String, level As UserAccount.AccountLevel)
+
+    End Sub
+
+    Sub LoginUser(username As String, password As String)
+
+    End Sub
+
+    Sub ChangeAccountLevel(userID As Integer, newLevel As UserAccount.AccountLevel)
+
+    End Sub
+
+    ' Dispose
+    Public Sub Dispose()
+        conn.Close()
+    End Sub
+
+End Class
--- CISK 503/CISK 503/Form Objects/formMain.Designer.vb
+++ CISK 503/CISK 503/Form Objects/formMain.Designer.vb
@@ -22,17 +22,57 @@ Partial Class formMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabControl1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Form1
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(710, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 24)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(710, 294)
+        Me.TabControl1.TabIndex = 1
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(702, 268)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Login Page"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'formMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(710, 318)
-        Me.Name = "Form1"
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "formMain"
         Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
 End Class
