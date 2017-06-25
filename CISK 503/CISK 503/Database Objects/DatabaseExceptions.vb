5a950acea87bacf3dd896f0cb6b9c71ca9de5965
Brian <bjcombs@gmail.com>
2017-06-25 11:48:34 -0500

    Added exceptions class for database

---
+++ CISK 503/CISK 503/Database Objects/DatabaseExceptions.vb
@@ -0,0 +1,13 @@
+﻿Public Class DatabaseExceptions
+    Inherits ApplicationException
+
+    Public Class LoginException
+        Inherits ApplicationException
+        Public Sub New(Optional message As String = "The login failed")
+            MyBase.New(message)
+        End Sub
+    End Class
+
+
+
+End Class
